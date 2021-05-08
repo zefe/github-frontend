f024bb02daf9df6bfdb21b072fc9290a0b7717ac
zefe <zefetorres@icloud.com>
2021-05-07 22:17:22 -0500

    add repository and user details view, also add Navbar

---
+++ src/Views/RepositoryView.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const RepositoryView = () => {
+    return (
+        <div className="container mt-5">
+            <h1>Search Repositories</h1>
+        </div>
+    )
+}
---
+++ src/Views/UserView.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const UserView = () => {
+    return (
+        <div className="container mt-5">
+            <h1>Search Users</h1>
+        </div>
+    )
+}
